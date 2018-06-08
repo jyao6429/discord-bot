fe321b6fcb50e6728e436a581254a3d9bccb8f33
jyao6429 <27450071+jyao6429@users.noreply.github.com>
2018-06-08 19:48:29 -0400

    More formatting

--- discord-bot/src/main/java/bot/Command.java
+++ discord-bot/src/main/java/bot/Command.java
@@ -5,10 +5,7 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 public interface Command
 {
 	boolean called(String[] args, MessageReceivedEvent event);
-
 	void action(String[] args, MessageReceivedEvent event);
-
 	String help();
-
 	void executed(boolean success, MessageReceivedEvent event);
 }
